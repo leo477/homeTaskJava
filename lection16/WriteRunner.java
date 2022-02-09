1e30b85748dd1a87653fa978315c9ebf009efbf0
Dmytro Zakharchenko <zaharr3@gmail.com>
2022-02-09 19:40:52 +0200

    Add File.separator

--- lection16/WriteRunner.java
+++ lection16/WriteRunner.java
@@ -5,8 +5,8 @@ import java.io.*;
 public class WriteRunner {
     public static void main(String[] args) {
         String pArray[]={"2020", "Java SE 13"};
-        File fByte = new File("data\\byte.data");
-        File fSymb = new File("data\\symbol.txt");
+        File fByte = new File("data"+ File.separator +"byte.data");
+        File fSymb = new File("data"+ File.separator +"symbol.txt");
         try(FileOutputStream fos=new FileOutputStream(fByte);
             FileWriter fw= new FileWriter(fSymb)){
 
