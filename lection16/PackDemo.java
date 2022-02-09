b9e65f936bac30f7bf031f5b196a1a1408dbbc3a
Dmytro Zakharchenko <zaharr3@gmail.com>
2022-02-09 22:04:46 +0200

    Create Start Class for Archivation catalog

--- lection16/PackDemo.java
+++ lection16/PackDemo.java
@@ -4,9 +4,9 @@ import java.io.FileNotFoundException;
 
 public class PackDemo {
     public static void main(String[] args) {
-        String dirName="";
-        PackJar pj= new PackJar("example.jar");
-        try{
+        String dirName = "";
+        PackJar pj = new PackJar("example.jar");
+        try {
             pj.pack(dirName);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
