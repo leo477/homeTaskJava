056dd2194bcdae1a5fdf0bd3db1a6be4f44e3905
Dmytro Zakharchenko <zaharr3@gmail.com>
2022-02-09 22:02:16 +0200

    Changed Example for Archivation catalog

--- lection16/PackJar.java
+++ lection16/PackJar.java
@@ -1,31 +1,53 @@
 package com.itea.dimka.lection16;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+import java.util.zip.Deflater;
 
 public class PackJar {
     private String jarFileName;
-    public final int BUFFER=2_048;
+    public final int BUFFER = 2_048;
 
     public PackJar(String jarFileName) {
         this.jarFileName = jarFileName;
     }
 
-    public void pack(String dirName) throws FileNotFoundException{
+    public void pack(String dirName) throws FileNotFoundException {
         File dir = new File(dirName);
-        if(!dir.exists() || !dir.isDirectory()){
-            throw  new FileNotFoundException(dir + " not found");
+        if (!dir.exists() || !dir.isDirectory()) {
+            throw new FileNotFoundException(dir + " not found");
         }
-        File [] files = dir.listFiles();
+        File[] files = dir.listFiles();
         ArrayList<String> listFilesToJar = new ArrayList<>();
-        for(int i=0; i< files.length; i++){
-            if(!files[i].isDirectory()){
+        for (int i = 0; i < files.length; i++) {
+            if (!files[i].isDirectory()) {
                 listFilesToJar.add(files[i].getPath());
             }
         }
 
-        String[] temp ={};
-        String [] filesToJar = listFilesToJar.toArray(temp);
+        String[] temp = {};
+        String[] filesToJar = listFilesToJar.toArray(temp);
+
+        try (FileOutputStream fos = new FileOutputStream(jarFileName);
+             JarOutputStream jos = new JarOutputStream(fos)) {
+            byte[] buffer = new byte[BUFFER];
+            jos.setLevel(Deflater.DEFAULT_COMPRESSION);
+            for (int i = 0; i < filesToJar.length; i++) {
+                jos.putNextEntry(new JarEntry(filesToJar[i]));
+                try (FileInputStream in = new FileInputStream(filesToJar[i])) {
+                    int len;
+                    while ((len = in.read(buffer)) > 0) {
+                        jos.write(buffer, 0, len);
+                    }
+                    jos.closeEntry();
+                } catch (FileNotFoundException e) {
+                    System.err.println("File not found!" + e);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
