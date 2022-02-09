b79ee20c8d5fcc2851c9681bc80c6c54dd01d3bf
Dmytro Zakharchenko <zaharr3@gmail.com>
2022-02-09 20:21:42 +0200

    Create class DemoWriter

---
+++ lection16/DemoWriter.java
@@ -0,0 +1,27 @@
+package com.itea.dimka.lection16;
+
+import java.io.*;
+
+public class DemoWriter {
+    public static void main(String[] args) {
+        File f = new File("data\\res.txt");
+        double[] v = {1.10,1.2,1.401, 5.01,6.017,7,8};
+        FileWriter fw=null;
+        BufferedWriter bw=null;
+        PrintWriter pw = null;
+        try{
+            fw=new FileWriter(f,true);
+            bw=new BufferedWriter(fw);
+            pw= new PrintWriter(bw);
+            for(double version:v){
+                pw.printf("Java %.2g %n", version);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (pw != null){
+                pw.close();
+            }
+        }
+    }
+}
