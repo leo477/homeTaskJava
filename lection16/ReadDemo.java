2801d0b0d8955c791be276986df67d140d3dd6a4
Dmytro Zakharchenko <zaharr3@gmail.com>
2022-02-09 19:27:02 +0200

    Change Class ReadDemo

--- lection16/ReadDemo.java
+++ lection16/ReadDemo.java
@@ -1,19 +1,16 @@
 package com.itea.dimka.lection16;
 
 import javax.imageio.IIOException;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class ReadDemo {
     public static void main(String[] args) {
         String file = "data\\file.txt";
         File f = new File(file);
         int b, count = 0;
-        FileReader fileReader = null;
-        try {
-            fileReader = new FileReader(f);
+
+        try (FileInputStream fileReader = new FileInputStream(new File(file))){
+            //fileReader = new FileReader(f);
             while ((b = fileReader.read()) != -1) {
                 System.out.println((char) b);
                 count++;
@@ -25,14 +22,6 @@ public class ReadDemo {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (fileReader != null) {
-                    fileReader.close();
-                }
-            } catch (IOException e) {
-                System.err.println("Close error: " + e);
-            }
         }
     }
 
