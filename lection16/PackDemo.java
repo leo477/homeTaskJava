19796618db90263193e7af751a9de10d74f9ca80
Dmytro Zakharchenko <zaharr3@gmail.com>
2022-02-09 22:03:55 +0200

    Create Start Class for Archivation catalog

---
+++ lection16/PackDemo.java
@@ -0,0 +1,15 @@
+package com.itea.dimka.lection16;
+
+import java.io.FileNotFoundException;
+
+public class PackDemo {
+    public static void main(String[] args) {
+        String dirName="";
+        PackJar pj= new PackJar("example.jar");
+        try{
+            pj.pack(dirName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
