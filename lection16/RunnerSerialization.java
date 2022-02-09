bef602b25213c5b882da59dcf2e3150cfe67def3
Dmytro Zakharchenko <zaharr3@gmail.com>
2022-02-09 21:28:58 +0200

    Add Runner Class for Serialization

--- lection16/RunnerSerialization.java
+++ lection16/RunnerSerialization.java
@@ -5,15 +5,15 @@ import java.io.InvalidObjectException;
 
 public class RunnerSerialization {
     public static void main(String[] args) {
-        Student ob= new Student("MMF","Goncharenko",1,"Pass1");
+        Student ob = new Student("MMF", "Goncharenko", 1, "Pass1");
         System.out.println(ob);
-        String file="data" + File.separator + "demo.data";
-        Serializator sz= new Serializator();
-        boolean b= sz.serialization(ob,file);
-        Student.faculty="GEO";
-        Student res=null;
-        try{
-            res=sz.deserialization(file);
+        String file = "data" + File.separator + "demo.data";
+        Serializator sz = new Serializator();
+        boolean b = sz.serialization(ob, file);
+        Student.faculty = "GEO";
+        Student res = null;
+        try {
+            res = sz.deserialization(file);
         } catch (InvalidObjectException e) {
             e.printStackTrace();
         }
