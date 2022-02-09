2333b49d04b551b30a202fe73635b1191e6c4694
Dmytro Zakharchenko <zaharr3@gmail.com>
2022-02-09 19:34:06 +0200

    Add Class WriteRunner

---
+++ lection16/WriteRunner.java
@@ -0,0 +1,25 @@
+package com.itea.dimka.lection16;
+
+import java.io.*;
+
+public class WriteRunner {
+    public static void main(String[] args) {
+        String pArray[]={"2020", "Java SE 13"};
+        File fByte = new File("data\\byte.data");
+        File fSymb = new File("data\\symbol.txt");
+        //FileOutputStream fos = null;
+        //FileWriter fw = null;
+        try(FileOutputStream fos=new FileOutputStream(fByte);
+            FileWriter fw= new FileWriter(fSymb)){
+
+            for (String a : pArray){
+                fos.write(a.getBytes());
+                fw.write(a);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
