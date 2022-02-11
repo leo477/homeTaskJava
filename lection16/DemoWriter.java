e064d3449fda68da4ae70851ed4c94095ddb4293
Dmytro Zakharchenko <zaharr3@gmail.com>
2022-02-11 02:52:49 -0800

    Test Change

--- lection16/DemoWriter.java
+++ lection16/DemoWriter.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class DemoWriter {
     public static void main(String[] args) {
-        File f = new File("data\\res.txt");
+        File f = new File("data" + File.separator + "res.txt");
         double[] v = {1.10,1.2,1.401, 5.01,6.017,7,8};
         FileWriter fw=null;
         BufferedWriter bw=null;
