eaf0bcd2d87ca45869eec65c357df86110801c7c
Dmytro Zakharchenko <zaharr3@gmail.com>
2022-02-17 14:52:56 +0200

    Lection 16 - Task 8 Change

--- lection16/hometask/Task8.java
+++ lection16/hometask/Task8.java
@@ -7,10 +7,13 @@ public class Task8 {
         BufferedReader bufferedReader = new BufferedReader(new FileReader("data" + File.separator + "spaceTest.java"));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("data" + File.separator + "data1" + File.separator + "spaceTextOutput.txt"));
         String line;
-        while ((line= bufferedReader.readLine())!=null){
-            bufferedWriter.write(line.replaceAll("\\b(\\s {2,})","\s").replaceAll("^\\B (\\s {5,})","\t"));
+        while ((line = bufferedReader.readLine()) != null) {
+            boolean flag = line.matches("(.*)public(.*)|(.*)private(.*)|(.*)protected(.*)|(.*)package(.*)|(.*)import(.*)");
+            if (flag)
+                bufferedWriter.write(line.replaceAll("\\b(\\s{2,})", "\s").replaceAll("\\s{2,}", "\t"));
+            else
+                bufferedWriter.write(line.replaceAll("\\b(\\s{2,})", "\s").replaceAll("^(?!\\s{16})^(\\s{8}).\\s{2,}", "\t"));
             bufferedWriter.newLine();
-            //System.out.println();
         }
         bufferedWriter.close();
     }
